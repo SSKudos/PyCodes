11568b582bca4fcb7ba8853ff0d12ff9cce9cc2b
SSKudos <74167601+SSKudos@users.noreply.github.com>
2021-04-21 20:21:05 +0100

    Create KPywhatKit1.py

---
+++ KPywhatKit1.py
@@ -0,0 +1,4 @@
+import pywhatkit
+
+#pywhatkit.headless_pyk.sendwhatmsg("+2347038896377", "GoodEvening", 2, 10)
+pywhatkit.sendwhatmsg("+234811", "Python testing dont worry you are not hacked", 20, 10)
